35f574d4d9d97130793f1b459d72c79167a39ed1
Gemcila <ggemcila@gmail.com>
2021-02-04 12:24:46 -0800

    Added "addEventListener" to roll button (new random image every time I click on the roll button)

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//Generate random number from 1 to 6
-const randomNo = Math.floor(Math.random()*6)+1;
-//Imagedice 1 to 6
-const diceImage = 'img/dice'+ randomNo + '.png';
-document.querySelectorAll('img')[0].setAttribute('src', diceImage);
+const rollButton = document.getElementById('roll');
+rollButton.addEventListener("click", () => {
+    const randomNo = Math.floor(Math.random()*6)+1;
+    const diceImage = 'img/dice'+ randomNo + '.png';
+    document.querySelectorAll('img')[0].setAttribute('src', diceImage);
+});
 
-const button = document.getElementById("roll");
-     
