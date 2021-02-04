636853f8d32cc06f3eef0883057f9082bd1eef52
Gemcila <ggemcila@gmail.com>
2021-02-04 12:46:12 -0800

    Added message "You lost" and "Roll"

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const rollButton = document.getElementById('roll');
+ 
+
 rollButton.addEventListener("click", () => {
     const randomNo = Math.floor(Math.random()*6)+1;
     const diceImage = 'img/dice'+ randomNo + '.png';
     document.querySelectorAll('img')[0].setAttribute('src', diceImage);
+    if (randomNo == 1) {
+        document.querySelector('h1').innerHTML = 'You lost!';
+    }else{
+        document.querySelector('h1').innerHTML = 'Roll';  
+    }
 });
 
+ 
