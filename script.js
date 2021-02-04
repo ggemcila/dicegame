c4b24f465b6a9117ee150f7eb18f989a9995c5bb
Gemcila <ggemcila@gmail.com>
2021-02-04 14:27:28 -0800

    Score is summing up each time I click

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let rollButton = document.getElementById('roll');
+let score =0;
 
 function game() 
 {
+
     const randomNo = Math.floor(Math.random()*6)+1;
     const diceImage = 'img/dice'+ randomNo + '.png';
     document.querySelectorAll('img')[0].setAttribute('src', diceImage);
+    score += randomNo;
+    document.getElementById('score').textContent = 'Score:' + score;
+
     if (randomNo == 1) {
         document.querySelector('h1').innerHTML = 'You lost!';
     }else {
         document.querySelector('h1').innerHTML = 'Roll';  
-        document.querySelector('#score').innerHTML = 'Score: ' + randomNo;
     }
 };
-
 rollButton.addEventListener("click", game);
- 
