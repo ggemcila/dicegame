3a1916767b014b8539a2ca84e6915e3e97de57c7
Gemcila <ggemcila@gmail.com>
2021-02-04 14:38:32 -0800

    Added winning condition (score>20)

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ function game()
     document.getElementById('score').textContent = 'Score:' + score;
 
     if (randomNo == 1) {
-        document.querySelector('h1').innerHTML = 'You lost!';
-    }else {
+        document.querySelector('h1').innerHTML = 'You lost!';     
+    }else if(score <= 20) {
         document.querySelector('h1').innerHTML = 'Roll';  
+    }else{
+        document.querySelector('h1').innerHTML = 'Winner!!!';         
     }
 };
+
 rollButton.addEventListener("click", game);
