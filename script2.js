f29aca148f719240697998091f80922782e96252
Gemcila <ggemcila@gmail.com>
2021-02-04 22:40:08 -0800

    Added 2 dice images (dices change every time I refresh the webpage)

--- script2.js
+++ script2.js
@@ -1,3 +1,8 @@
+const firstRandomNum = Math.floor(Math.random()*6)+1;
+const diceImage1 = 'img/dice'+ firstRandomNum + '.png';
+document.querySelectorAll('img')[0].setAttribute('src', diceImage1);
 
-
-     
+const secondRandomNum = Math.floor(Math.random()*6)+1;
+const diceImage2 = 'img/dice'+ secondRandomNum + '.png';
+document.querySelectorAll('img')[1].setAttribute('src', diceImage2);
+   
