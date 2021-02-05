e3658576657fbf3032fa1cc8dc3ffe47ad5248c7
Gemcila <ggemcila@gmail.com>
2021-02-04 18:06:57 -0800

    Added "start again" button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let rollButton = document.getElementById('roll');
 let score =0;
+let startButton = document.getElementById('start');
+startButton.style.visibility= 'hidden';
 
 function game() 
 {
-
     const randomNo = Math.floor(Math.random()*6)+1;
     const diceImage = 'img/dice'+ randomNo + '.png';
     document.querySelectorAll('img')[0].setAttribute('src', diceImage);
@@ -11,13 +12,20 @@ function game()
     document.getElementById('score').textContent = 'Score:' + score;
 
     if (randomNo == 1) {
-        document.querySelector('h1').innerHTML = 'You lost!';     
+        document.querySelector('h1').innerHTML = 'You lost!';  
+        startButton.style.visibility= 'visible';
+    
     }else if(score < 20) {
-        document.querySelector('h1').innerHTML = 'Roll';  
+        document.querySelector('h1').innerHTML = 'Roll';   
     }else{
-        document.querySelector('h1').innerHTML = 'Winner!!!';  
+        document.querySelector('h1').innerHTML = 'Winner!!!';
+        startButton.style.visibility= 'visible';  
         score = 0;              
     }
 };
 
-rollButton.addEventListener("click", game);
+
+rollButton.addEventListener("click", game,);
+startButton
+
+
