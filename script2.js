f0bbb07d14b018bd3d943dc4c556b8a14061e4cf
Gemcila <ggemcila@gmail.com>
2021-02-04 22:47:00 -0800

    Score sum up for both players

--- script2.js
+++ script2.js
@@ -1,8 +1,20 @@
-const firstRandomNum = Math.floor(Math.random()*6)+1;
-const diceImage1 = 'img/dice'+ firstRandomNum + '.png';
-document.querySelectorAll('img')[0].setAttribute('src', diceImage1);
+let rollButton = document.getElementById('roll');
+let score =0;
 
-const secondRandomNum = Math.floor(Math.random()*6)+1;
-const diceImage2 = 'img/dice'+ secondRandomNum + '.png';
-document.querySelectorAll('img')[1].setAttribute('src', diceImage2);
-   
+function game() {
+    const firstRandomNum = Math.floor(Math.random()*6)+1;
+    const diceImage1 = 'img/dice'+ firstRandomNum + '.png';
+    document.querySelectorAll('img')[0].setAttribute('src', diceImage1);
+    score += firstRandomNum;
+    document.getElementById('score1').textContent = 'Score:' + score;
+
+    const secondRandomNum = Math.floor(Math.random()*6)+1;
+    const diceImage2 = 'img/dice'+ secondRandomNum + '.png';
+    document.querySelectorAll('img')[1].setAttribute('src', diceImage2);
+    score += secondRandomNum;
+    document.getElementById('score2').textContent = 'Score:' + score;
+};
+
+
+rollButton.addEventListener("click", game);
+ 
