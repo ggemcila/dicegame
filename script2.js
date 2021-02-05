b6119fb6fa6503a32802127df48b2f44f618861b
Gemcila <ggemcila@gmail.com>
2021-02-04 21:55:52 -0800

    Added score board

--- script2.js
+++ script2.js
@@ -1,8 +1,3 @@
-//Generate a random number from 1 to 6
-const firstRandomNum = Math.floor(Math.random()*6)+1;
-const secondRandomNum = Math.floor(Math.random()*6)+1;
-// image/dice
-const firstDiceImage = 'img/dice' + firstRandomNum + '.png';
-document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage);
-const secondDiceImage = 'img/dice' + secondRandomNum + '.png';
-document.querySelectorAll('img')[1].setAttribute('src',secondDiceImage);
+
+
+     
