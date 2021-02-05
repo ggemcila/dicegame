893050ab9b39e092f1864bff9def9e17c7a47a33
Gemcila <ggemcila@gmail.com>
2021-02-04 21:15:39 -0800

    Defined 2 random numbers to display random dices

---
+++ script2.js
@@ -0,0 +1,8 @@
+/*Generate a random number from 1 to 6
+const firstRandomNum = Math.floor(Math.random()*6)+1;
+const secondRandomNum = Math.floor(Math.random()*6)+1;
+// image/dice
+const firstDiceImage = 'img/dice' + firstRandomNum + '.png';
+document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage);
+const secondDiceImage = 'img/dice' + secondRandomNum + '.png';
+document.querySelectorAll('img')[0].setAttribute('src',secondDiceImage);
